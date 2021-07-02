6383ddc766da323c9546dd9d5acf1becea074c5f
Isantanna <igor_sant@hotmail.com>
2021-07-02 13:53:46 -0300

    Progressão do projeto 2

---
+++ ByteBank/ByteBank/OperacaoFinanceiraException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    public class OperacaoFinanceiraException : Exception
+    {
+        public OperacaoFinanceiraException()
+        {
+
+        }
+
+        public OperacaoFinanceiraException(String mensagem) : base(mensagem)
+        {
+
+        }
+
+        public OperacaoFinanceiraException(String mensagem, Exception innerException) : base(mensagem, innerException)
+        {
+
+        }
+    }
+}
