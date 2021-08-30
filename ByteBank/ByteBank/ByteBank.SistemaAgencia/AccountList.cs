39a208e99010944436e19ecc3112b591f346d152
Isantanna <igor_sant@hotmail.com>
2021-08-30 18:30:56 -0300

    Adição de sistema de lista de contas

---
+++ ByteBank/ByteBank/ByteBank.SistemaAgencia/AccountList.cs
@@ -0,0 +1,35 @@
+﻿using ByteBank.Model;
+using System;
+
+namespace ByteBank.SistemaAgencia
+{
+    public class AccountList
+    {
+        private ContaCorrente[] _itens;
+        private int _numeroDeContas;
+        private int _nextPosition;
+
+        public AccountList()
+        {
+            _itens = new ContaCorrente[_numeroDeContas];
+        }
+        public void Add(ContaCorrente item)
+
+        {
+            if (_nextPosition <= _numeroDeContas)
+            {
+                _numeroDeContas++;
+                
+                Console.WriteLine("Aumento de posição");
+
+                return;
+            }
+            else
+            {
+                _itens[_nextPosition] = item;
+                _nextPosition++;
+            }
+        }
+
+    }
+}
--- ByteBank/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBank/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿using ByteBank.Model;
+using System;
 using System.Text.RegularExpressions;
-using ByteBank.Model;
-using Humanizer;
 
 namespace ByteBank.SistemaAgencia
 {
@@ -10,29 +9,46 @@ namespace ByteBank.SistemaAgencia
         static void Main(string[] args)
 
         {
-            //Buscador de expressões.
-            string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
-            UrlValueExtractor valueExtractor = new UrlValueExtractor(url);
+            AccountList Contas = new AccountList();
 
-            string value = valueExtractor.GetValueParam("moedaDestino");
-            Console.WriteLine("Valor de moedaDestino: " + value);
-            
-            string valueOrigin = valueExtractor.GetValueParam("moedaOrigem");
-            Console.WriteLine("Valor de moedaOrigem: " + valueOrigin);
+            Contas.Add(new ContaCorrente(001, 1001));
+            Contas.Add(new ContaCorrente(001, 1002));
+            Contas.Add(new ContaCorrente(001, 1003));
+            Contas.Add(new ContaCorrente(001, 1004));
+            Contas.Add(new ContaCorrente(001, 1005));
+            Contas.Add(new ContaCorrente(001, 1006));
+            Contas.Add(new ContaCorrente(001, 1007));
 
+
+            //for (int i = 0; i < Contas.Length; i++)
+            //{
+            //    Console.WriteLine($"Conta {i} {Contas[i].Numero}");
+            //}
+
+            Console.ReadLine();
+        }
+
+        public void DefaultFinder()
+        {
             //Buscador de padrões.
             string regularText = "Para entrar em contato sobre vaga de emprego ligue (21) 97232-6937, Isantanna";
             string cellphoneDefault = "[(][0-9]{2}[)] ?[0-9]{4,5}-?[0-9]{4}";
 
             Match found = Regex.Match(regularText, cellphoneDefault);
             Console.WriteLine(found);
+        }
 
+        public void ExpressionFinder()
+        {
+            //Buscador de expressões.
+            string url = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar";
+            UrlValueExtractor valueExtractor = new UrlValueExtractor(url);
 
-            ContaCorrente account = new ContaCorrente(001,0003);
+            string value = valueExtractor.GetValueParam("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + value);
 
-            Console.WriteLine(account.Agencia);
-
-            Console.ReadLine();
+            string valueOrigin = valueExtractor.GetValueParam("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valueOrigin);
         }
     }
 }
